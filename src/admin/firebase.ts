7767c6a9e93ea7575af0a1e89042abe098bd3156
Arnar Páll Birgisson <arnarp@gmail.com>
2018-10-04 20:27:01 +0000

    Not load firebase on hmr

--- src/admin/firebase.ts
+++ src/admin/firebase.ts
@@ -1,16 +1,18 @@
-import { initializeApp, firestore as fire, auth as a } from 'firebase/app'
+import { initializeApp, firestore as fire, auth as a, apps } from 'firebase/app'
 import 'firebase/auth'
 import 'firebase/firestore'
 import 'firebase/messaging'
 
-const config = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.FIREBASE_DATABASE_URL,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+if (apps.length === 0) {
+  const config = {
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  }
+  initializeApp(config)
 }
-initializeApp(config)
 
 export const firestore = fire()
 firestore.settings({ timestampsInSnapshots: true })
