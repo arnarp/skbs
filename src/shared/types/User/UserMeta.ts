a2435699fc987c7f16f8d52e293b8f00da613d7c
Arnar Páll Birgisson <arnarp@gmail.com>
2019-01-27 00:11:35 +0000

    Fixing unneccesary import

--- src/shared/types/User/UserMeta.ts
+++ src/shared/types/User/UserMeta.ts
@@ -1,23 +1,20 @@
 import { Omit, Overwrite } from '../utils'
 import { UserClaims } from './UserClaims'
-import { firestore } from 'firebase/app'
 
-export type UserMeta =
-  Readonly<{
-    id: string
-    claimsRefreshTime: Date
-    claims: UserClaims
-    displayName: string
-    photoURL: string
-    email: string
-  }>
+export type UserMeta = Readonly<{
+  id: string
+  claimsRefreshTime: Date
+  claims: UserClaims
+  displayName: string
+  photoURL: string
+  email: string
+}>
 
 export type UserMetaDocument = Omit<UserMeta, 'id'>
-
 
 export type NewUserMetaDocument = Overwrite<
   UserMetaDocument,
   {
-    claimsRefreshTime:  firestore.FieldValue
+    claimsRefreshTime: firebase.firestore.FieldValue
   }
 >
